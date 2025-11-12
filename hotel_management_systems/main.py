5d0b8ec1886e46bb13a43f786d3fe7c0f20c0957
kashiftareen <kashif.tareen1999@gmail.com>
2025-11-12 15:20:03 +0500

    Initial commit: setup Hospital Management System project structure with FastAPI, SQLAlchemy, Alembic

---
+++ hotel_management_systems/main.py
@@ -0,0 +1,20 @@
+from fastapi import FastAPI 
+import uvicorn
+
+
+
+
+app =FastAPI(
+    title="Hotel Management System", 
+    description="API documentation for hotel management",
+    version="1.0.0",
+)
+
+
+
+
+
+
+#to start and reload server auto
+def start():
+    uvicorn.run("hotel_management_systems.main:app",host="127.0.0.1",port=5656,reload=True)
