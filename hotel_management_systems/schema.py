eb0801f2c0dcf682e0e6f00314e3b128f4f2e942
kashiftareen <kashif.tareen1999@gmail.com>
2025-11-14 13:57:47 +0500

    feat(rooms): add full CRUD endpoints for rooms with JWT authentication

--- hotel_management_systems/schema.py
+++ hotel_management_systems/schema.py
@@ -1,5 +1,6 @@
 from pydantic import BaseModel
 from enum import Enum
+from typing import Optional
 
 # define RoomStatus Enum
 class RoomStatus(Enum):
@@ -15,3 +16,14 @@ class Create_User(BaseModel):
 # Schema for updating customer details
 class update_cust(Create_User):
     pass
+
+# To create Room Schema
+class create_room(BaseModel):
+    number:str
+    price:float
+    status:RoomStatus
+
+class update_room(BaseModel):
+    number: Optional[str] = None
+    price: Optional[float] = None
+    status: Optional[RoomStatus] = None
